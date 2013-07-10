a98a8589ee652654e0d9024e6ed18d80cf8aa586
SubhadipChatterjee <subhadip.chatterjee@tcs.com>
2013-07-10 21:03:38 +0530

    added custom ItemWriter and delegation feature

--- src/main/java/poc/springbatch/beans/PersonFinderQuerySetter.java
+++ src/main/java/poc/springbatch/beans/PersonFinderQuerySetter.java
@@ -6,6 +6,8 @@ package poc.springbatch.beans;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.PreparedStatementSetter;
 
 /**
@@ -13,7 +15,8 @@ import org.springframework.jdbc.core.PreparedStatementSetter;
  * @author subhadip.chatterjee@tcs.com
  */
 public class PersonFinderQuerySetter implements PreparedStatementSetter {
-
+    
+    private final Logger logger = LoggerFactory.getLogger(getClass());    
     private String firstName;
 
     public String getFirstName() {
@@ -26,6 +29,9 @@ public class PersonFinderQuerySetter implements PreparedStatementSetter {
 
     @Override
     public void setValues(PreparedStatement ps) throws SQLException {
-        ps.setString(1, '%' + firstName + '%');
+        if (logger.isInfoEnabled()) {
+                logger.info("Input to query->[firstName]=" + firstName);
+            }
+        ps.setString(1, firstName);
     }
 }
---
+++ src/main/java/poc/springbatch/beans/PersonUpdateQuerySetter.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poc.springbatch.beans;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Date;
+import java.util.Calendar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import poc.springbatch.entities.Person;
+
+/**
+ *
+ * @author subhadip.chatterjee@tcs.com
+ */
+public class PersonUpdateQuerySetter implements PreparedStatementSetter {
+    
+    private final static Logger logger = LoggerFactory.getLogger(PersonUpdateQuerySetter.class);
+    private Person person;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public void setValues(PreparedStatement ps) throws SQLException {
+        if (logger.isInfoEnabled()) {
+            logger.info("PreparedStatementSetter.setValues() begins...");
+        }
+        if (person == null) {
+            throw new RuntimeException();
+        }
+        ps.setString(1, person.getId());        
+    }
+}
